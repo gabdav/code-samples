014451cc2835dc5e665c172141f641354ee8983b
Gabi <gabipaldavid@gmail.com>
2019-08-15 19:10:14 +0300

    Initial commit

---
+++ BulletController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class BulletController : MonoBehaviour
+{
+    private ObjectPool objPool;
+    private Vector3 startPoint;
+    private int ammountAlreadySpawned;
+    private float chargeUpTimer = 0;
+    private Color color;
+    private bool chargeSoundisOn =false;
+    private bool chargingSoundisOn=false;
+    private List<GameObject> spawnedProjectiles = new List<GameObject>();
+    private float deathBoxTimer=0.5f;
+    private bool miniBoom = false;
+
+    [Header("Projectile Settings")]
+    public int numberOfProjectiles;
+    public float projectileSpeed;
+    public GameObject ProjectilePrefab;
+    public float radius = 1f;
+    public Transform parentSpawner;
+    public GameObject particleExplosion;
+    public GameObject miniParticleExplosion;
+    public GameObject chargeUp;
+    public int ammountOfCharge = 0;
+    public bool release = false;
+    public float maxCharge = 100;
+    public bool timeToBlow = false;
+    public GameObject DeathBoxMB;
+    public CamerShake cameraShake;
+    public int charges;
+
+    public TextMeshProUGUI textCounter;
+     void Start()
+    {
+        objPool = ObjectPool.Instance;
+        
+    }
+
+    void FixedUpdate() {
+        MoveProjectiles(spawnedProjectiles);
+    }
+
+    void Update()
+    {
+        textCounter.text = charges.ToString();
+        if (Time.timeScale != 0 && charges>0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                release = false;
+                if (ammountAlreadySpawned <= ammountOfCharge)
+                {
+                    color = color * Time.deltaTime;
+                    GameObject vfxCharge = Instantiate(chargeUp, transform.position, Quaternion.identity);
+                    vfxCharge.transform.SetParent(parentSpawner);
+                    Destroy(vfxCharge, vfxCharge.GetComponent<ParticleSystem>().main.duration);
+                    ammountAlreadySpawned++;
+                    if(!chargingSoundisOn){
+                    FindObjectOfType<AudioManager>().Play("Charging");  
+                    chargingSoundisOn = true;
+                    }                  
+                }
+                chargeUpTimer += Time.deltaTime;
+                if(chargeUpTimer>= maxCharge && !chargeSoundisOn){
+
+                    FindObjectOfType<AudioManager>().Play("ChargedUp");
+                    chargeSoundisOn =true;
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                ammountAlreadySpawned = 0;
+                release = true;
+                 FindObjectOfType<AudioManager>().Stop("ChargedUp");
+                 FindObjectOfType<AudioManager>().Stop("Charging");                   
+                 chargeSoundisOn = false;
+                 chargingSoundisOn = false;
+                if (chargeUpTimer >= maxCharge)
+                {
+                    timeToBlow = true;
+                    FindObjectOfType<AudioManager>().Play("Boom");
+                    StartCoroutine(cameraShake.Shake(.15f,.1f));
+                    startPoint = transform.position;
+                    spawnedProjectiles = SpawnProjectile(numberOfProjectiles);
+                    GameObject vfx = Instantiate(particleExplosion, transform.position, Quaternion.identity);
+                    vfx.transform.SetParent(parentSpawner);
+                    Destroy(vfx, vfx.GetComponent<ParticleSystem>().main.duration);
+                    charges--;
+                }
+                else
+                {
+                    timeToBlow = false;
+                }
+                chargeUpTimer = 0;
+            }
+            else
+            {
+                timeToBlow = false;
+
+            }
+            if(Input.GetKeyDown(KeyCode.E) && !Input.GetKey(KeyCode.Space)){
+                miniBoom = true;
+                deathBoxTimer = 0.5f;
+                DeathBoxMB.SetActive(true);
+                startPoint = transform.position;
+                FindObjectOfType<AudioManager>().Play("MiniBoom");
+                spawnedProjectiles = SpawnProjectile(numberOfProjectiles);
+                GameObject vfx = Instantiate(miniParticleExplosion, transform.position, Quaternion.identity);
+                vfx.GetComponent<ParticleSystem>().main.duration.Equals(vfx.GetComponent<ParticleSystem>().main.duration/2);
+                vfx.transform.SetParent(parentSpawner);
+                 Destroy(vfx, vfx.GetComponent<ParticleSystem>().main.duration);
+                charges--;
+            }
+                
+        }
+         if(miniBoom && DeathBoxMB.activeSelf){
+                    deathBoxTimer-= Time.deltaTime;
+                    if(deathBoxTimer <= 0){
+                    DeathBoxMB.SetActive(false);
+                    miniBoom = false;
+             }
+         }
+    }
+
+
+
+    private List<GameObject> SpawnProjectile(int _numberOfProjectiles)
+    {   
+        List<GameObject> projectiles = new List<GameObject>();
+        for(int i = 0; i<= _numberOfProjectiles - 1; i++)
+        {
+            GameObject tmpObj = objPool.SpaqnFromPool("Projectile", startPoint, Quaternion.identity);
+            tmpObj.transform.SetParent(parentSpawner);
+            tmpObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            projectiles.Add(tmpObj);
+        }
+        return projectiles;
+    }
+
+    private void MoveProjectiles(List<GameObject> projectiles){
+        float angleStep = 360f / projectiles.Count;
+         float angle = 0f;
+         foreach(GameObject tmpObj in projectiles){
+            float projectileDirXPos = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirYPos = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+            Vector3 projectileVector = new Vector3(projectileDirXPos, projectileDirYPos, 0);
+            Vector3 projectileMoveDir = (((projectileVector - startPoint).normalized * projectileSpeed) * Time.fixedDeltaTime);
+            tmpObj.GetComponent<Rigidbody>().velocity = new Vector3(projectileMoveDir.x, 0, projectileMoveDir.y);
+            angle += angleStep;
+         }
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+    Camera viewCamera;
+    public float moveSpeed = 5;
+    PlayerController controller;
+    public Renderer rend;
+    public Color p;
+    public Light lt;
+
+    public float maxIntensity = 2.0f;
+    public float minIntensity = 0f;
+    public float pulseSpeed = 1;
+
+    private float targetIntensity = 2.0f;
+    private float currentIntensity;
+
+    public Lose enemy;
+
+    void Start () {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+        rend = GetComponent<Renderer>();
+        lt = GetComponent<Light>();
+      
+    }
+	
+	void Update () {
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);//returns the position of the mouse with a ray
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance)) // asigns to rayDistance ray
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Quaternion targetRotation = Quaternion.LookRotation(point - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+
+        }
+
+        if (!controller.hit)
+        {
+            Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+            controller.Move(moveVelocity);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                currentIntensity = Mathf.MoveTowards(p[2], targetIntensity, Time.deltaTime * pulseSpeed);
+                if (currentIntensity >= maxIntensity)
+                {
+                    currentIntensity = maxIntensity;
+
+                }
+
+            }
+            else
+            {
+                currentIntensity = Mathf.MoveTowards(currentIntensity, minIntensity, Time.deltaTime * pulseSpeed * 2); ;
+            }
+
+        lt.intensity = currentIntensity*2;
+        p[1] = currentIntensity / 2;
+        p[2] = currentIntensity;
+        rend.material.SetColor("_EmissionColor", p);
+        }
+        else
+        {
+
+            currentIntensity = maxIntensity;
+            lt.intensity = currentIntensity * 2;
+            p[0] = currentIntensity;
+            p[1] = 0;
+            p[2] = 0;
+            rend.material.SetColor("_EmissionColor", p);
+        }
+
+    }
+
+ }
